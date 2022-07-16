f548fed887603c109a17f4927a5e5d872525cbc8
Daniel Gorbatov <dzgorbatov@gmail.com>
2022-07-16 09:31:49 -0700

    finish login page

--- lib/src/views/ui/Login.dart
+++ lib/src/views/ui/Login.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:sushi_supervise/ScoutingLib/logic/size/ScreenSize.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -12,6 +14,10 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  int? teamNum;
+  String? eventCode;
+  String? password;
+
   @override
   Widget build(BuildContext context) {
     var colors = Theme.of(context);
@@ -20,9 +26,121 @@ class _LoginState extends State<Login> {
       width: ScreenSize.width,
       height: ScreenSize.height * 0.9,
       child: Column(children: [
-        SizedBox(
-          width: ScreenSize.width,
-          height: ScreenSize.height * 0.6,
+        Padding(
+          padding: EdgeInsets.only(top: ScreenSize.height * 0.08, bottom: ScreenSize.height * 0.05),
+          child: SizedBox(
+            width: ScreenSize.width,
+            height: ScreenSize.height * 0.47,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: ScreenSize.width * 0.75,
+                    height: ScreenSize.height * 0.07,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              width: ScreenSize.height * 0.006,
+                              color: colors.primaryColorDark),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              width: ScreenSize.height * 0.006,
+                              color: colors.primaryColorDark),
+                        ),
+                        hintText: "TEAM #",
+                        hintStyle: TextStyle(color: colors.primaryColorDark),
+                        isDense: true,
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: ScreenSize.height * 0.005),
+                      ),
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.mohave(
+                          textStyle: TextStyle(
+                        fontSize: ScreenSize.width * 0.07,
+                        color: colors.primaryColorDark,
+                      )),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly
+                      ],
+                      onChanged: (String? val) => setState(() {
+                        teamNum = (val != null ? int.parse(val) : val) as int?;
+                      }),
+                    ),
+                  ),
+                  SizedBox(
+                    width: ScreenSize.width * 0.75,
+                    height: ScreenSize.height * 0.07,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              width: ScreenSize.height * 0.006,
+                              color: colors.primaryColorDark),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              width: ScreenSize.height * 0.006,
+                              color: colors.primaryColorDark),
+                        ),
+                        hintText: "EVENT CODE",
+                        hintStyle: TextStyle(color: colors.primaryColorDark),
+                        isDense: true,
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: ScreenSize.height * 0.005),
+                      ),
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.mohave(
+                          textStyle: TextStyle(
+                        fontSize: ScreenSize.width * 0.07,
+                        color: colors.primaryColorDark,
+                      )),
+                      keyboardType: TextInputType.text,
+                      onChanged: (String? val) => setState(() {
+                        eventCode = val;
+                      }),
+                    ),
+                  ),
+                  SizedBox(
+                    width: ScreenSize.width * 0.75,
+                    height: ScreenSize.height * 0.07,
+                    child: TextFormField(
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              width: ScreenSize.height * 0.006,
+                              color: colors.primaryColorDark),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              width: ScreenSize.height * 0.006,
+                              color: colors.primaryColorDark),
+                        ),
+                        hintText: "PASSWORD",
+                        hintStyle: TextStyle(color: colors.primaryColorDark),
+                        isDense: true,
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: ScreenSize.height * 0.005),
+                      ),
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.mohave(
+                          textStyle: TextStyle(
+                        fontSize: ScreenSize.width * 0.07,
+                        color: colors.primaryColorDark,
+                      )),
+                      keyboardType: TextInputType.visiblePassword,
+                      onChanged: (String? val) => setState(() {
+                        password = val;
+                      }),
+                    ),
+                  ),
+                ]),
+          ),
         ),
         SizedBox(
           width: ScreenSize.width,
@@ -33,7 +151,7 @@ class _LoginState extends State<Login> {
                 "./assets/images/banner.svg",
                 width: ScreenSize.width,
               ),
-              Padding(
+              if (teamNum != null && eventCode != null && password != null) Padding(
                 padding: EdgeInsets.only(top: ScreenSize.height * 0.185),
                 child: Container(
                     width: ScreenSize.width,
